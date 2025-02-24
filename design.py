4f2038fb8e60726f2c8b6481e169a80e3f2b33ad
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-02-24 20:01:19 +0300

    Create design.py

---
+++ design.py
@@ -0,0 +1,20 @@
+from PyQt6 import QtCore, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 450)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(500, 400, 80, 30))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git Circles"))
+        self.pushButton.setText(_translate("MainWindow", "Рисовать"))
